52a4abf0792bfff02f22b667fa46065f2f780177
Michal Valoušek <michal@plovarna.cz>
2012-07-29 22:23:23 +0200

    Verze 2

--- app1/views.py
+++ app1/views.py
@@ -12,7 +12,14 @@ zubil 77? Pití už prostě otisk, ni? Skvrnu mi máry medicíně chtěj hranice
 myslela s vydral: čte cikánů a žide zaškrtit čapnou podala akty rusy lidská, ty
 po zadělá vy berkovec zabývá. Sítě téže, roh 77 řku zády chabě u nebyl. Podpisy
 tě slávy z autor i bezvýznamné pevně výbušný sedli ex a divné vrypu přines
-sebebohatší zdrceně bývá. Míň přivítal, skel nic hodinky.'''
+sebebohatší zdrceně bývá. Míň přivítal, skel nic hodinky. Bílý brejlemi herecké
+no. Hm peru ex kouta vyjde, koňak omámen úst plných. Ryb hmatem psi hromada
+třpytil. Póvl ta trefit zrosený přimlouvalajá. Ve se ponětí mne žárlivý ó čistá
+mé jo již jedna zaplakal.  Zardívaje my za nám huso? Oč he i pří stromě he
+lampu, mít stu ta les pud, si pešť nám chovat. Baret hadr eh jeti blahé dny
+čekati, vůl máte dá tvou vezl řeč sviňská prezident nepořádnou drbal. Říkal jo
+třeskl zachází, pyšně dá lumpa ona žena oběhli. He 77 nič píchlo lože ní krvi
+kolej vás bývala? Lázní au bál?'''
 SENTENCES = [i.strip().replace('\n', ' ') for i in SENTENCES.split('.') if i.strip()]
 
 def app1_view(request):
