b57511eca07431226adcb23375221b7c249aee9a
Michal Valoušek <michal@plovarna.cz>
2012-09-10 17:36:40 +0200

    Ladeni media souboru

--- demoproj/settings.py
+++ demoproj/settings.py
@@ -56,7 +56,7 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media/static')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/static/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -66,7 +66,7 @@ STATIC_ROOT = os.path.join(PROJECT_PATH, 'media/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/media/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
